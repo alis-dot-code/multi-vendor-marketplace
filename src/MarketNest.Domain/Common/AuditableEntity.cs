c85211230c20baf884a92103471305a77f4fa053
Ali Developer <aliasgharupwork60@gmail.com>
2024-06-12 14:45:00 +0500

    feat: add domain common types (BaseEntity, AuditableEntity, ISoftDeletable, Result, PagedResult)

---
+++ src/MarketNest.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,7 @@
+namespace MarketNest.Domain.Common;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
